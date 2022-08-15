02c026cdc72daf12dae2c402213ce2c7693f0624
Sam Olukotun <sam.olukotun@circleci.com>
2022-08-15 10:13:49 -0700

    Debug without status file

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -245,7 +245,7 @@ SetupLogs() {
 # This is done so this script may be tested.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
-    # . "/tmp/SLACK_JOB_STATUS"
+    # . "/tmp/SLACK_JOB_STATUS"  # Is this file correctly formatted for Windows? Missing EOF?
     ShouldPost
     SetupEnvVars
     SetupLogs
